ddf9bad1cf714ddb9780a0e4df315d054b8e527b
namancristvats <namancristvats@gmail.com>
2026-07-10 23:17:35 +0530

    Initial Commit

---
+++ inventory-service/src/main/java/com/codingshuttle/ecommerce/inventory_service/client/OrdersFeignClient.java
@@ -0,0 +1,11 @@
+package com.codingshuttle.ecommerce.inventory_service.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@FeignClient(name="order-service",path="/orders")
+public interface OrdersFeignClient {
+
+    @GetMapping("/core/helloOrders")
+    public String helloOrders();
+}
